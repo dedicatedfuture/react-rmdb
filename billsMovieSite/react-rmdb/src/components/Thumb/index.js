413f326cfce2711220a460ef9615fc04a6a1128b
Bill Zerega <zerega85@gmail.com>
2021-10-26 09:57:59 -0400

    added header and thumb click through features

--- billsMovieSite/react-rmdb/src/components/Thumb/index.js
+++ billsMovieSite/react-rmdb/src/components/Thumb/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 //styles
 
@@ -6,7 +7,14 @@ import { Image } from './Thumbs.styles';
 
 const Thumb = ({ image, movieId, clickable }) => (
     <div>
-        <Image src={image} alt='movie-thumb' />
+        {clickable ? (
+            <Link to={`/${movieId}`}>
+                <Image src={image} alt='movie-thumb' />
+            </Link>
+        ) : (
+            <Image src={image} alt='movie-thumb' />
+        )};
+        
     </div>
 );
 
